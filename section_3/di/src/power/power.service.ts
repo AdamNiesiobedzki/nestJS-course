1b1d1bb88d0fca04be75980c3d1c3adc6f71e5f9
AdamNiesiobedzki <adam.niesiobedzki@o2.pl>
2023-12-08 17:08:37 +0100

    initial commit

---
+++ section_3/di/src/power/power.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PowerService {
+
+    suplyPower(watts: number){
+        console.log('Supplying ' + watts + ' worth of power.');
+    }
+}
